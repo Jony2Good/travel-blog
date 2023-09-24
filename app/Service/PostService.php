204af93f14e91bf82e243a44530a6f97deda6317
artem_emelianenko <akk_12@mail.ru>
2023-09-24 21:56:38 +0300

    add:transactions

--- app/Service/PostService.php
+++ app/Service/PostService.php
@@ -5,34 +5,48 @@ namespace App\Service;
 use App\Http\Requests\Admin\Post\StoreRequest;
 use App\Http\Requests\Admin\Post\UpdateRequest;
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 
 class PostService
 {
     public function store(array $data, StoreRequest $request): void
     {
-        $tags = $data['tags'];
-        unset($data['tags']);
+        try {
+            Db::beginTransaction();
+            $tags = $data['tags'];
+            unset($data['tags']);
 
-        $data['prev_image'] = $request->file('prev_image')->store('uploads', 'public');
-        $data['main_image'] = $request->file('main_image')->store('uploads', 'public');
+            $data['prev_image'] = $request->file('prev_image')->store('uploads', 'public');
+            $data['main_image'] = $request->file('main_image')->store('uploads', 'public');
 
-        $post = Post::firstOrCreate($data);
-        $post->tags()->attach($tags);
+            $post = Post::firstOrCreate($data);
+            $post->tags()->attach($tags);
+            Db::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            abort(500);
+        }
     }
 
     public function update(array $data, UpdateRequest $request, Post $post)
     {
-        $tags = $data['tags'];
-        unset($data['tags']);
-        if (isset($data['prev_image'])) {
-            $data['prev_image'] = $request->file('prev_image')->store('uploads', 'public');
+        try {
+            Db::beginTransaction();
+            $tags = $data['tags'];
+            unset($data['tags']);
+            if (isset($data['prev_image'])) {
+                $data['prev_image'] = $request->file('prev_image')->store('uploads', 'public');
+            }
+            if (isset($data['main_image'])) {
+                $data['main_image'] = $request->file('main_image')->store('uploads', 'public');
+            }
+            $post->update($data);
+            $post->tags()->sync($tags);
+            Db::commit();
+        } catch(\Exception $e) {
+            Db::rollBack();
+            abort(500);
         }
-        if (isset($data['main_image'])) {
-            $data['main_image'] = $request->file('main_image')->store('uploads', 'public');
-        }
-        $post->update($data);
-        $post->tags()->sync($tags);
-
         return $post;
     }
 
